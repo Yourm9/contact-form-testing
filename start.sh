3f62ced1f626c0f649c0bad373b1bc10a0627cae
“yourm9” <Ivan.leonard@icloud.com>
2025-06-08 19:08:19 +0100

    Switch to gunicorn for production deployment

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 playwright install-deps
 python3 -m playwright install
-export FLASK_APP=app.py
-export FLASK_ENV=production
-flask run --host=0.0.0.0
+gunicorn app:app --bind 0.0.0.0:$PORT
