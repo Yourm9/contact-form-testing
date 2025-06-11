c08c90a51be2a521b3c816bf637af9ea560e1328
“yourm9” <Ivan.leonard@icloud.com>
2025-06-11 23:12:43 +0100

    Working local version with bug fixes

--- app.py
+++ app.py
@@ -45,5 +45,6 @@ def run_bot():
 
     return jsonify(results)
 
+
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5050)
